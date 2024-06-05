f6397a8b449129af03f9d9281e9651ac5257f8ec
Afnansayed <afnansayed1973@gmail.com>
2024-06-05 22:52:37 +0600

    get single data for deyails

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 //mongoDb connect
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khblnbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -56,6 +56,13 @@ async function run() {
       ]).toArray();
       console.log(popularCamps);
       res.send(popularCamps)
+    })
+    //get single camp data
+    app.get('/allCamps/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await campCollection.findOne(query);
+            res.send(result); 
     })
     //success story
     app.get('/successStory', async (req, res) => {
