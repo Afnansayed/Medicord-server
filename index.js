69936fbe9bcec3b52f98a483c520825fd3cc2023
Afnansayed <afnansayed1973@gmail.com>
2024-06-11 14:48:38 +0600

    get payment history by query

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const port = process.env.PORT || 5000;
 
 
 //middleware 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://medicoord-901e4.web.app',
+    'https://medicoord-901e4.firebaseapp.com'
+  ]
+}));
 app.use(express.json());
 
 const verifyToken = (req, res, next) => {
@@ -44,7 +50,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   // await client.connect();
 
     const usersCollection = client.db('medCoordDB').collection('users');
     const successCollection = client.db('medCoordDB').collection('successStory');
@@ -351,6 +357,14 @@ async function run() {
     })
 
     //for payment history
+    //get
+    app.get('/histories' , async (req,res) =>{
+         const email = req.query.email;
+         const query = {email: email};
+         const result = await paymentCollection.find(query).toArray();
+         res.send(result)
+    })
+    //post
     app.post('/histories', async(req,res) => {
             const history = req.body;
             const result = await paymentCollection.insertOne(history);
@@ -358,7 +372,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
